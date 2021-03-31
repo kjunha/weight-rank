5e34ea3ebadc940d6b8d932bec4b712d4705d8ec
김준하 <junha.kim@way2bit.com>
2021-03-31 18:33:56 +0900

    add adonis db migrations

---
+++ database/migrations/1617179217634_users_schema.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class UsersSchema extends Schema {
+  up () {
+    this.create('users', (table) => {
+      table.increments()
+      table.string('oauth_key').notNullable()
+      table.string('oauth_service').notNullable()
+      table.enu('user_status', ['ACTIVE', 'INACTIVE', 'DEREGISTER', 'SUSPENDED']).defaultTo('ACTIVE')
+      table.timestamps()
+      table.timestamp('deleted_at')
+    })
+  }
+
+  down () {
+    this.drop('users')
+  }
+}
+
+module.exports = UsersSchema
