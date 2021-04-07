a77d7cb394eec0e02c0541471284e38aca30f82b
김준하 <junha.kim@way2bit.com>
2021-04-07 14:24:02 +0900

    Redis 설정 및 연결 테스트 완료

--- api-server/app/Controllers/Http/UserController.js
+++ api-server/app/Controllers/Http/UserController.js
@@ -9,6 +9,16 @@ class UserController extends BaseController{
     return response.send(url);
   }
 
+  /**
+   * 응답코드
+   * 200: 정상적인 로그인 프로세스 진행
+   * 204: code oauth 코드가 정상적으로 수신되었으나, 해당 유저 없음
+   * 그 외: 정상적인 로그인이 이루어지지 않음 (코드 만료나 라우트 문제)
+   * @param ally
+   * @param auth
+   * @param response
+   * @returns {Promise<*>}
+   */
   async getAccessToken({ ally, auth, response }) {
     const user = await ally.driver('github').getUser()
     const token = user.getAccessToken()
@@ -17,6 +27,7 @@ class UserController extends BaseController{
       .where('email', email)
       .first()
     if(!userCheck) {
+      // returns 204 when user is not found
       return response.noContent('user_not_found')
     }
     return response.send(user.getAccessToken())
