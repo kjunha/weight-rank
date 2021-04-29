de5de7d68c8f047c39c9d61f07451907ddae7a15
Kwangok Ahn <dksrhkddhr06@gmail.com>
2021-04-29 12:26:22 +0900

    웹페이지 작업

---
+++ web-client/components/Navi.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="navigation">
+        <ul>
+            <li><a href="#">서비스 소개</a></li>
+            <li><a href="#">채용</a></li>
+            <li><a href="#">뉴스룸</a></li>
+            <li><a href="#">제휴문의</a></li>
+            <li><a href="#">FAQ</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style scoped>
+    @font-face {
+        font-family: 'IBMPlexSansKR-Regular';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/IBMPlexSansKR-Regular.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+    .navigation {
+        width: 550px;
+        display: flex;
+        align-items: center;
+    }
+    ul, li {
+        list-style: none;
+        margin: 0px;
+        padding-left: 0px;
+        font-weight: 700;
+        font-size: 16px;
+    }
+
+    li {
+        transition: transform .2s;
+    }
+
+    ul {
+        display: flex;
+        justify-content: space-between;
+        flex-grow: 1;
+    }
+
+    a {
+        text-decoration: none;
+        color: white;
+        transition: color 0.3s;
+    }
+
+    li:hover {
+        transform: scale(1.2);
+        
+    }
+    a:hover {
+        color: tomato;
+    }
+    
+</style>
--- web-client/pages/index.vue
+++ web-client/pages/index.vue
@@ -1,29 +1,29 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        web-client
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="wholesome">
+    <div class="main">
+      <div class="landing"></div>
+      <div class="top">
+        <div class="logo">
+          <Logo />
+        </div>
+        <div class="nav">
+          <Navi />
+        </div>
       </div>
     </div>
+    <section class="contents1">
+      <div class="main-contents">
+        <h1 style="font-weight:700;">나의 3대 운동은 <br>어느 정도일까?</h1>
+        <p>다른 사람들과 운동 루틴을 공유하고 <br>내 랭크가 얼마나 되는지 알아보자</p>
+        <div class="downloadlink1">
+          <img src="@/assets/GooglePlay.png" alt="googlePlay" width="180px">
+          <img src="@/assets/AppStore.png" alt="googlePlay" width="180px">
+        </div>
+      </div>
+    </section>
+    <section class="contents2">
+      another contents2
+    </section>
   </div>
 </template>
 
@@ -32,42 +32,57 @@ export default {}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .main {
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+    padding: 0;
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .landing {
+    z-index: -1000;
+    position: absolute;
+    top: 0;
+    width: 100vw;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  @media (min-width: 992px) {
+    .landing {
+      height: calc(100vh);
+      background-image: url(https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80);
+      background-position: 50%;
+    }
+  }
+
+  .top { 
+    height: 90px;
+    width: 950px;
+    display: flex; 
+    justify-content: space-between; 
+    margin-top: 10px;
+  }
+
+  .contents1 {
+    height: calc(100vh - 94px);
+  }
+
+  .main-contents {
+    color: white;
+    width: 950px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+  }
+
+  .downloadlink {
+    width: inherit;
+    display: flex;
+    flex-direction: row;
+  }
+
 </style>
